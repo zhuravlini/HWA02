5bb75696284767499e4608a1367ded25beb255f1
Tatiana Zhuravleva <zhuravlini@gmail.com>
2023-01-07 17:51:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/nit8ir4lann3na2d?svg=true)](https://ci.appveyor.com/project/zhuravlini/hwa02)
---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,34 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+
+//                .header("Content-Type", "application/json; charset=UTF-8")
+//                .contentType(ContentType.JSON)
+//                .body("", hasSize(3))
+//                .body("[1].currency", equalTo("USD"))
+//                .body("every {it.balance >= 0}", is(true))
+//                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
